67882c750aed7f01c4fcedcbc189e8cc95e4f2c7
huyboon <huyht0704@gmail.com>
2025-09-06 11:37:03 +0700

    chang banner

--- src/app/(guest)/services/ServicePageContent.tsx
+++ src/app/(guest)/services/ServicePageContent.tsx
@@ -81,6 +81,10 @@ const ServicePageContent = () => {
 			{/* phần trên */}
 			<motion.div
 				className="w-full h-[90vh] flex items-center justify-center"
+				style={{
+					backgroundImage: `url(/bannerservicepage.png)`,
+					backgroundPosition: "center",
+				}}
 				initial={{ opacity: 0 }}
 				animate={{ opacity: 1 }}
 				transition={{ duration: 0.5 }}
